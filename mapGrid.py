d3627f6f5321f71c9a66f944b5cfd09e4aa6477e
derf-dog <38248717+derf-dog@users.noreply.github.com>
2018-04-11 23:45:43 +0930

    Update mapGrid.py

--- mapGrid.py
+++ mapGrid.py
@@ -22,25 +22,23 @@ class MapGrid(object):
         for numLoop in range(10):
             self.myboard.append([0,0,0,0,0,0,0,0,0,0])
         
+       # load terrain to board
+        
+        # terrain map objects - co-ordinates - Aarons example map from Assignment 2
+        hard = [[0,4],[0,5],[1,1],[1,2],[1,7],[1,8],[2,1],[2,4],[2,5],[2,8],[4,2],[4,4],[4,5],[4,7],[4,9],
+               [5,0],[5,2],[5,4],[5,5],[5,7],[5,9],[7,1],[7,4],[7,5],[7,8],[8,1],[8,2],[8,7],[8,8],[9,4],[9,5]]
+        
+        soft = [[0,2],[0,7],[1,4],[1,5],[2,0],[2,3],[2,6],[2,9],[3,2],[3,7],[4,1],[4,8],[5,1],[5,8],[6,2],[6,7],
+                [7,0],[7,3],[7,6],[7,9],[8,4],[8,5],[9,2],[9,7]]
+        
+        
         # load terrain to board
-        self.myboard[1][1] = Terrain("soft")  
-        self.myboard[1][2] = Terrain("soft") 
-        self.myboard[1][3] = Terrain("hard")       
-        self.myboard[1][4] = Terrain("hard")
-        self.myboard[1][6] = Terrain("soft")
-        self.myboard[1][7] = Terrain("hard")
-        self.myboard[1][8] = Terrain("soft")
-        self.myboard[8][8] = Terrain("soft")
-        self.myboard[2][1] = Terrain("hard")
-        self.myboard[2][4] = Terrain("hard")
-        self.myboard[3][1] = Terrain("hard")
-        self.myboard[3][8] = Terrain("soft")
-        self.myboard[4][3] = Terrain("hard")
-        self.myboard[4][4] = Terrain("hard")
-        self.myboard[5][1] = Terrain("soft")
-        self.myboard[6][8] = Terrain("soft")
-        self.myboard[7][8] = Terrain("soft")
-    
+        for each in hard:
+            self.myboard[each[0]][each[1]] = Terrain("hard")
+        
+        for each in soft:
+            self.myboard[each[0]][each[1]] = Terrain("soft")
+                
     
     # get the object in the grid reference
     def getGridObject(self, position, x, y):
