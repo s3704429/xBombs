e1005f88cf462e6e7e6fb1501dfd6c6ab4a4a528
derf-dog <38248717+derf-dog@users.noreply.github.com>
2018-04-24 12:21:28 +0930

    Update playerKeys.py

--- playerKeys.py
+++ playerKeys.py
@@ -1,59 +1,48 @@
-'''
-Created on 10 Apr. 2018
-
-@author: bob
-'''
 import pygame
 from bomb import *
+
 
 def playerKeys(keypress, myboard, player1, player2, bombs):
     # for player 1 movement
     if keypress[pygame.K_UP]:
-        if player1.position[0] > 0 and myboard.getGridObject(player1.position, 0, -1) == 0:
-            player1.position[0] -= 1
+        player1.moveUp(myboard)
+                
             
     if keypress[pygame.K_DOWN]:
-        if player1.position[0] < 9 and myboard.getGridObject(player1.position, 0, 1) == 0:
-            player1.position[0] += 1
-            
+        player1.moveDown(myboard)
             
     if keypress[pygame.K_LEFT]:
-        if player1.position[1] > 0 and myboard.getGridObject(player1.position, -1, 0) == 0: 
-            player1.position[1] -= 1
+        player1.moveLeft(myboard)
          
     if keypress[pygame.K_RIGHT]:
-        if  player1.position[1] < 9 and myboard.getGridObject(player1.position, 1, 0) == 0:
-            player1.position[1] += 1
+        player1.moveRight(myboard)
     
     if keypress[pygame.K_KP0]:
         if player1.bombsTotal > 0:
-            myboard.myboard[player1.position[0]][player1.position[1]] = Bomb('soft', 45, 1, player1, player1.position)
-            bombs.append(player1.position);
-            player1.bombsTotal -= 1
-    
+            player1.dropBomb(myboard, bombs)
+            
+    if keypress[pygame.K_UP] != True and keypress[pygame.K_RIGHT] != True and keypress[pygame.K_LEFT] != True and keypress[pygame.K_DOWN] != True:
+        player1.standing = True
     
     # for player 2 movement
     if keypress[pygame.K_w]:
-        if player2.position[0] > 0 and myboard.getGridObject(player2.position, 0, -1) == 0:
-            player2.position[0] -= 1
+        player2.moveUp(myboard)
             
     if keypress[pygame.K_s]:
-        if player2.position[0] < 9 and myboard.getGridObject(player2.position, 0, 1) == 0:
-            player2.position[0] += 1
-            
+        player2.moveDown(myboard)            
             
     if keypress[pygame.K_a]:
-        if player2.position[1] > 0  and myboard.getGridObject(player2.position, -1, 0) == 0:
-            player2.position[1] -= 1
+        player2.moveLeft(myboard)
          
             
     if keypress[pygame.K_d]:
-        if  player2.position[1] < 9 and myboard.getGridObject(player2.position, 1, 0) == 0:
-            player2.position[1] += 1
+        player2.moveRight(myboard)
                 
                 
     if keypress[pygame.K_q]:
         if player2.bombsTotal > 0:
-            myboard.myboard[player2.position[0]][player2.position[1]] = Bomb('soft', 45, 1, player2, player2.position)
-            bombs.append(player2.position);
-            player2.bombsTotal -= 1
+            player2.dropBomb(myboard, bombs)
+
+    if keypress[pygame.K_w] != True and keypress[pygame.K_a] != True and keypress[pygame.K_s] != True and keypress[pygame.K_d] != True:
+        player2.standing = True
+    
