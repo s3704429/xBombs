1e13c2463df9738e00780eba90523875726879d2
derf-dog <38248717+derf-dog@users.noreply.github.com>
2018-04-30 15:23:59 +0930

    Update playerKeys.py

--- playerKeys.py
+++ playerKeys.py
@@ -18,8 +18,7 @@ def playerKeys(keypress, myboard, player1, player2, bombs):
         player1.moveRight(myboard)
     
     if keypress[pygame.K_KP0]:
-        if player1.bombsTotal > 0:
-            player1.dropBomb(myboard, bombs)
+        player1.dropBomb(myboard, bombs)
             
     if keypress[pygame.K_UP] != True and keypress[pygame.K_RIGHT] != True and keypress[pygame.K_LEFT] != True and keypress[pygame.K_DOWN] != True:
         player1.standing = True
@@ -40,8 +39,7 @@ def playerKeys(keypress, myboard, player1, player2, bombs):
                 
                 
     if keypress[pygame.K_q]:
-        if player2.bombsTotal > 0:
-            player2.dropBomb(myboard, bombs)
+        player2.dropBomb(myboard, bombs)
 
     if keypress[pygame.K_w] != True and keypress[pygame.K_a] != True and keypress[pygame.K_s] != True and keypress[pygame.K_d] != True:
         player2.standing = True
