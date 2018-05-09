dd59f60783cfcb51ed71b2186007bce98faaa117
SaltySumo <38693505+SaltySumo@users.noreply.github.com>
2018-05-09 21:03:37 +1000

    Update powerup.py

--- powerup.py
+++ powerup.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 #from main import * 
 
@@ -7,27 +8,36 @@ class Powerup(object):
     classdocs
     '''
 
-
     def __init__(self, material, time):
         '''
         Constructor
         '''
         self.material = material # string
         self.fuse = time # int
-        self.image = 'images/blast.png'
 
-    def drop_powerup(screen, x,y):
-
-	    blastImage = pygame.image.load('images/blast.png')
+        randomSelection=random.randint(0,2)
+        if randomSelection==0:
+            self.image = 'images/blast.png'
+            self.selectedPowerup="blastRadiusUp"
+        elif randomSelection==1:
+            self.image = 'images/extrabomb.png'
+            self.selectedPowerup="bombTotalUp"
+        elif randomSelection==2:
+            self.image = 'images/speed.png'
+            self.selectedPowerup="speedUp"
+        # etc for the rest of the powerups (limits of RNG will need to be 
+        # incresed/decreased depending on how many there are)
 
     def blast(x,y):
 
-        screen.blit(blastImage, (x,y))
+        screen.blit(powerupImage, (x,y))
 
-	#blast(x,y)
     
+    # when player picksup run this function.
     def grabPowerup(self, player):
-        player.bombStrength += 1
-        player.speed += 1
-
-#drop_powerup(screen, (indexY)*CELLSIZE,(indexX-explode)*CELLSIZE)
+        if self.selectedPowerup=="blastRadiusUp":
+            player.bombStrength += 1
+        elif self.selectedPowerup=="bombTotalUp":
+            player.bombsTotal += 1
+        elif self.selectedPowerup=="speedUp":
+            player.speed += 1
